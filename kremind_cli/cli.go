d13ca412e0841a67f7a19b37966a6178d5c99599
Simon <brotherlogic@gmail.com>
2024-09-26 20:14:47 -0700

    Working on the client

---
+++ kremind_cli/cli.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"time"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
+	pb "github.com/brotherlogic/mdb/proto"
+)
+
+func main() {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Minute*60)
+	defer cancel()
+
+	conn, err := grpc.Dial(os.Args[1], grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("Bad dial: %v", err)
+	}
+
+	client := pb.NewMDBServiceClient(conn)
+
+	if os.Args[2] == "list" {
+
+	}
+
+}
